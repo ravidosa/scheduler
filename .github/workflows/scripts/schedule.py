cc908ce0c4e6c33926e5666833f2963eb263d5af
ravi <55117331+ravidosa@users.noreply.github.com>
2025-06-07 03:43:29 -0700

    .

---
+++ .github/workflows/scripts/schedule.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import pause
+from datetime import datetime, timedelta
+from multiprocessing import Pool
+
+d = datetime.today().replace(hour=16, minute=0, second=0, microsecond=500)
+t = timedelta((12 - d.weekday()) % 7)
+d += t
+
+print(d)
+
+cookie = os.environ["COOKIE"]
+
+avail_dic = {3: [2, 3],
+             4: [2, 3, 4, 5, 6, 7],
+             5: [2, 3, 4, 5, 6, 7],
+             6: [3, 4, 6, 7],
+             7: [3, 5, 6, 7],
+             8: [2, 3, 5, 6, 7]}
+
+def schedule(shift):
+    hour, day = shift
+    url = f"https://prv.tutor.com/nGEN/Tools/ScheduleManager_v2/SchedulerWorker.aspx?Type=Set&Week={d.month}/{d.day}/{d.year}&WeekDay={day}&Hour={hour}AM"
+    x = requests.post(url, headers = {"Cookie": cookie})
+    print(x.text)
+
+if __name__ == '__main__':
+    pool = Pool()
+    pause.until(d)
+    pool.map(schedule, [(hour, day) for hour in avail_dic for day in avail_dic[hour]])
+    pool.close()
