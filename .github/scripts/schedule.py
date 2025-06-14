2fbfcc26d47549c6591190aff7297fa64c487387
ravi <55117331+ravidosa@users.noreply.github.com>
2025-06-14 09:32:38 -0700

    test

--- .github/scripts/schedule.py
+++ .github/scripts/schedule.py
@@ -4,28 +4,30 @@ import pause
 from datetime import datetime, timedelta
 from multiprocessing import Pool
 
-d = datetime.today().replace(hour=11, minute=25, second=0, microsecond=500)
+d = datetime.today().replace(hour=11, minute=0, second=0, microsecond=500)
 t = timedelta((12 - d.weekday()) % 7)
 d += t
 
 print(d)
+print(datetime.today() + timedelta((12 - d.weekday()) % 7))
 
 cookie = os.environ["COOKIE"]
 print(cookie)
 
-avail_dic = {3: [2, 3],
-             4: [2, 3, 4, 5, 6, 7],
-             5: [2, 3, 4, 5, 6, 7],
-             6: [3, 4, 6, 7],
-             7: [3, 5, 6, 7],
-             8: [2, 3, 5, 6, 7]}
+avail_dic = {"5AM": [7],
+             "6AM": [7],
+             "8AM": [4],
+             "9AM": [2, 4, 5, 6],
+             "10AM": [2, 4, 5, 6],
+             "11AM": [4, 5],
+             "12PM": [3, 4, 5, 6, 7],
+             "1PM": [2, 3, 4, 5, 6]}
 
 def schedule(shift):
     hour, day = shift
-    url = f"https://prv.tutor.com/nGEN/Tools/ScheduleManager_v2/SchedulerWorker.aspx?Type=Set&Week={d.month}/{d.day}/{d.year}&WeekDay={day}&Hour={hour}AM"
+    url = f"https://prv.tutor.com/nGEN/Tools/ScheduleManager_v2/SchedulerWorker.aspx?Type=Set&Week={d.month}/{d.day}/{d.year}&WeekDay={day}&Hour={hour}"
     x = requests.post(url, headers = {"Cookie": cookie})
-    print("OS = ", os.environ.keys())
-    print(x.headers)
+    print(x.text)
 
 if __name__ == '__main__':
     pool = Pool()
